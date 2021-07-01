961199cc789d51bd9c14f77bbdbdbac5897cfffa
Ella <ella444@gmail.com>
2021-07-01 21:10:34 +0300

    edit README

--- README.md
+++ README.md
@@ -1,15 +1,35 @@
 # Hackathon
 
-The research:
-The research this project was created for is taking place in Dr Jason Friedman's Lab. The aim of this research is to determine wethear musical practice on the piano helps to improve motor symptoms in PD patients. The patients will go through 6 weeks of piano practice (guided and unguided), as well as some examinations to test their motor abilities. the examinations will take place at the begining and the end of the 6-week session, except one (QDG) that will be preformed weekly.
-during this time the patients will have a keyboard at home, connected to an arduino device that will record their playing and store it as csv files of midi data. there will be a new file each day.
+## The research:
+The research this project was created for is taking place in Dr Jason Friedman's Lab. 
+The aim of this research is to determine whether musical practice on the piano helps to improve motor symptoms in PD patients. 
+The patients will go through 6 weeks of piano practice (guided and unguided), as well as some examinations to test their motor abilities. 
+The examinations will take place at the beginning and the end of the 6-week session, except one (QDG) that will be preformed weekly.
+During this time the patients will have a keyboard at home, connected to an arduino device that will record their playing and store it as csv files of midi data. 
+There will be a new file each day.
 
-The project:
-The aim of this project was to provide a tool to read and analyze the data from the piano, including divide  several playing sessions that were held in the same day, show statistical measures for the QDG test and play a chosen part, all this in an accesible gui.
+## The Project:
+The aim of this project was to provide a tool to read and analyze the data from the piano.
+It includes dividing the data into several playing sessions that were held in the same day, 
+show statistical measures for the QDG test and play a chosen part, all this in an accesible gui.
 
-Project components:
-- Notes diaplay: allows dinamic display of the playing data (bars representing time and duration of each note that was played), that can be zoomed in and out. in the chosen range, it's possible to export a csv file of the specific data and statistical analysis.
-- Statistical analysis: calculates 3 importent measures of the QDG test: note duration, press velocity (intensity, how strong was the keybord pressed) and frequency (taps per minute).
-- playing: converts the data back to midi and allows to play a chosen part.
+## Project components:
+- GUI: allows dynamic display of the recorded data, the graph element can be zoomed in and out and panned. 
+* Browse for subjects data folder
+* Choose between different subjects, days and sessions (defined as a minimum of 30 minutes break)
+* Zoom and Pan
+* Display the chosen session on the graph
+* In the chosen range:
+  ** Display Statistical analysis data and updated in real time.
+  ** Play selected range
+  ** Export the chosen data to a csv file of the specific data
 
-The gui itself was designed so that the user can navigate between the different subjects, days and sessions easily. once a seesion is chosen, the gui presents the notes display. there are dedicated buttons to play the music displayed and to export the data as described.
+- Statistical analysis: calculates 3 important measures of the QDG test: note duration, press velocity (intensity, how strong was the keyboard pressed) and frequency (taps per minute).
+
+- Play Data: use the Play button to convert the data back to midi and play it.
+
+
+#installation and Run
+- In Windows run the 'run.bat' file. it will first install the requirements using pip and then run the Gui.
+- Note: It might be necessary to adjust the file according to the python installed on the machine.
+- python >= 3.8 is required. 
--- midi.py
+++ midi.py
@@ -5,12 +5,13 @@ from midiutil.MidiFile import MIDIFile
 
 
 def main_playmidi(csv_midi: pd.DataFrame):
-    '''
-
+    """
+        the main function that calls different functions that convert
+        the quantitive data back to midi and plays it.
 
         parameters: dataframe of the quantitive data
         output: playing the midi file
-        '''
+        """
     csv_to_midi(csv_midi)
     midi_file = 'midifile.mid'
 
@@ -36,13 +37,12 @@ def main_playmidi(csv_midi: pd.DataFrame):
 
 def csv_to_midi(csvdata):
     """
-    converts the data from the csv file into a midi file.
-    checks the time, duration and sound volume (velocity) of each note
-    and generates a new midi file containing this information
-        parameters: dataframe of the quantitive data
-        returns: a midi file
-    """
-
+        converts the data from the csv file into a midi file.
+        checks the time, duration and sound volume (velocity) of each note
+        and generates a new midi file containing this information
+            parameters: dataframe of the quantitive data
+            returns: a midi file
+        """
     midi_data = calculate_duration(csvdata)
 
     track = 0
@@ -50,7 +50,8 @@ def csv_to_midi(csvdata):
     time = 0
     tempo = 60  # In BPM -  use 60 so that time is just in seconds
 
-    mymidi = MIDIFile(1)  # One track, defaults to format 1
+    mymidi = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+    # automatically)
     mymidi.addTempo(track, time, tempo)
 
     for index, row in midi_data.iterrows():
@@ -65,8 +66,13 @@ def csv_to_midi(csvdata):
 
 
 def play_music(midi_file):
-    '''
-
+    """
+    loads and plays a midi file, using a package of playing music.
+    """
+    clock = pygame.time.Clock()
+    try:
+        pygame.mixer.music.load(midi_file)
+        print("Music file %s loaded!" % midi_file)
     except pygame.error:
         print("File %s not found! (%s)" % (midi_file, pygame.get_error()))
         return
@@ -77,52 +83,14 @@ def play_music(midi_file):
 
 
 def calculate_duration(csvdata: pd.DataFrame):
-    '''
+    """
     allows the rearange the quantitive midi data;
     in the original file, there are different rows to pressing and releasing the keyboard.
     this function creates one row of data for each note played including the start time and duration.
     it also converts the start time to UTC time, which is the only way a midi file can be generated.
         parameters: dataframe with the original data
         returns: new dataframe contaning the above-mentioned data.
-    '''
-    csvdata, headers = Utils.df_convert_time(csvdata)
-    csvdata['timestamp'] = csvdata[['datetime']].apply(lambda x: x[0].timestamp(), axis=1) # UTC time
-    # dataframe to hold data of each note before the we have data of the releasing time.
-    notes_data = pd.DataFrame(np.zeros(shape=[88,5]), index=[np.arange(21,109)], columns=['starttime','starttime_utc','note','duration','velocity']) 
-    # dataframe to save the new data into
-    new_data = pd.DataFrame(columns=['starttime','starttime_utc','note','duration','velocity'])
-
-    startsong = csvdata['timestamp'][0]-0.001
-
-    for index, row in csvdata.iterrows():
-        # on each iteration, the loop either adds a new line to notes_data with the start time data
-        # (action = 1), or calculates the duration and adds a new line to the new_data df.
-        # in notes_data, the info is stored according to the note midi symbol (numbered 21 to 108)
-        # so it'll be easy to recognize each note's closer.
-        note = row['note']
-        if row['action'] == 1:
-            if int(notes_data.loc[note,'starttime_utc']) == 0:
-                notes_data.loc[note,'starttime'] = row['time']
-                notes_data.loc[note,'starttime_utc'] = row['timestamp']-startsong
-                notes_data.loc[note,'note'] = row['note']
-                notes_data.loc[note,'velocity'] = row['velocity']
-        elif row['action'] == 2:
-            if int(notes_data.loc[note,'starttime_utc'] != 0):
-                notes_data.loc[note,'duration'] = row['timestamp']-startsong-notes_data.loc[note,'starttime_utc']
-                new_line = notes_data.loc[note]
-                new_data = new_data.append(new_line,ignore_index=True)
-                notes_data.loc[note,:] = np.zeros(shape=(1,5))
-    new_data = new_data.sort_values('starttime').reindex()
-    return new_data
-
-def calculate_duration(csvdata: pd.DataFrame):
-        allows the rearange the quantitive midi data;
-        in the original file, there are different rows to pressing and releasing the keyboard.
-        this function creates one row of data for each note played including the start time and duration.
-        it also converts the start time to UTC time, which is the only way a midi file can be generated.
-            parameters: dataframe with the original data
-            returns: new dataframe contaning the above-mentioned data.
-        '''
+    """
     notes_data = pd.DataFrame(np.zeros(shape=[88, 5]), index=[np.arange(21, 109)],
                               columns=['starttime', 'starttime_utc', 'note', 'duration', 'velocity'])
     new_data = pd.DataFrame(columns=['starttime', 'starttime_utc', 'note', 'duration', 'velocity'])
